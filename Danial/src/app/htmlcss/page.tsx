741d1c6d863c52b1d7125429de73f394e457828c
danialshafique <danialmian359@gmail.com>
2024-12-11 16:46:09 -0700

    comment

--- Danial/src/app/htmlcss/page.tsx
+++ Danial/src/app/htmlcss/page.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+// import React from 'react'
+// import Image from 'next/image'
+// import Link from 'next/link'
 
 
 
 const HtmlCss = () => {
   return (
-   <main className="animate-fadeIn h-auto w-[100vw] lg:pb-96 pb-[1300px]">
+  //  <main className="animate-fadeIn h-auto w-[100vw] lg:pb-96 pb-[1300px]">
     {/* <div className='h-auto'> */}
       {/* <h1 className='text-center font-extrabold mt-10 text-2xl'>{'< Html/ Css/ JavaScript />'}</h1> */}
       {/* <div className='grid lg:grid-cols-2 grid-cols-1 lg:mx-48 mx-20 sm:mx-40 md:mx-56 gap-10 h-[400px] mt-10'> */}
@@ -85,7 +85,7 @@ const HtmlCss = () => {
         </div> */}
       {/* </div> */}
     {/* // </div> */}
-   </main>
+  //  </main>
   )
 }
 
